2af9fddb7ddd5496647e751690246b7fc628bb1d
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-09-01 13:19:34 +0000

    descrição das alterações realizadas

--- receitas.py
+++ receitas.py
@@ -34,3 +34,17 @@ receitas = [
         'ingredientes':['500 g de farinha de trigo','1 copo de água',' 50 g de manteiga',' 1 colher de café de sal','25 g de fermento para pão','1 ovo','1 colher de sopa de fermento em pó','100 g de molho de tomate','1 linguiça calabresa','200 g de queijo mussarela ','orégano a gosto,rodelas de tomate e cebola .azeitona a gosto']
         'modo de preparo':['Misture 50 g de farinha, fermento e um pouco de água.Deixe descansar por 20 minutos. Adicione os demais ingredientes e amasse até obter uma massa bem macia.Corte em 3 partes iguais, forme 3 bolas e cubra-as com um pano.Deixe descansar por 1 hora. Abra a massa com um rolo até obter 25 cm de diâmetro. Espalhe o molho de tomate por cima e leve para assar em forno pré-aquecido, a 180ºC, por 15 minutos.Retire e deixe esfriar.Espalhe a linguiça sobre o molho de tomate e cubra com a cebola e o tomate.Coloque o queijo mussarela por baixo da linguiça.Decore com a azeitona e orégano.Retorne ao forno por mais 10 minutos ou até a mussarela dourar.']
     }
+]
+    
+@app.get("/")
+def hello():
+    return{"title":"Livro de receitas"}
+
+@app.get("/receitas/{receita}")
+def get_receita(receita:str):
+    for r in receitas:
+        if r["nome"].lower() == receita.lower():
+            return r
+    return {"error": "Receita não encontrada"}
+
+    
