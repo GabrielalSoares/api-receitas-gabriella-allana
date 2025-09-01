e9f96759f471fd64a1ef91a5291c33321d583a4f
Gabriela Soares <gabriela.rebeca05@aluno.ifce.edu.br>
2025-09-01 11:55:35 +0000

    descrição das alterações realizadas

---
+++ receitas.py
@@ -0,0 +1,20 @@
+receitas = [
+    {
+        'nome': 'brigaeiro',
+        'ingredientes': ['1 colher de sopa de manteiga', '1 lata ou 395 gramas de leite condensado', '30 gramas de chocolate em pó', '150 gramas de granulado de chocolate'],
+        'modo de preparo': 'Em uma panela, mistura o leite condensado, chocolate em pó e manteiga. Cozinhe em fogo médio e mexa até que o brigadeiro comece a desgrudar da panela. deixe esfriar e faça pequenas bolas com a mão passando a massa no chocolate granulado.'
+
+    },
+    {
+
+        'nome': 'palha italiana',
+        'ingredientes': ['1 lata de leite condensado', '1/2 colher (sopa) de margarina', '8 colheres (sopa) de chocolate em pó', '1 pacote de biscoito maisena'],
+        'modo de preparo': ['Pique o biscoito em pedacinhos pequenos e reserve. Com o leite condensado, a margarina e o chocolate em pó, faça um brigadeiro. Assim que o brigadeiro começar a soltar do fundo da panela, misture o biscoito picado até formar uma massa, retire do fogo. Unte uma bancada de mármore, ou alguma superfície lisa, com margarina e despeje essa massa. Abra a massa, batendo com a palma das mãos. Deixe esfriar e corte em quadradinhos.']
+
+    },
+    {
+
+         'nome': 'torta de frango',
+         'ingredientes':['500 g de peito de frango sem pele', '4 colheres (sopa) de óleo', '1 cebola picada', '1 xícara (chá) de ervilhas', 'pimenta-do-reino a gosto', '1/2 litro de caldo de galinha', '1 dente de alho amassado', '3 tomates sem pele e sem sementes', 'sal a gosto e pimenta', '250 ml de leite', '2 ovos','
+'sal e queijo ralado na gosto', '3/4 de xícara (chá) de óleo', '1 e 1/2 xícara (chá) de farinha de trigo', '1 colher (sopa) de fermento em pó'.] 
+        'modo de preparo': ['Cozinhe o peito de frango no caldo até ficar macio. Separe 1 xícara (chá) de caldo do cozimento e reserve. Refogue os demais ingredientes e acrescente as ervilhas por último. Desfie o frango, misture ao caldo e deixe cozinhar até secar. Bata o leite, o óleo e os ovos no liquidificador em velocidade baixa. Acrescente aos poucos a farinha, o sal e o fermento. Despeje metade da massa em uma forma untada e adicione o recheio sobre ela. Cubra com o restante de massa e o queijo ralado. Leve ao forno preaquecido (180° C) até dourar.']
