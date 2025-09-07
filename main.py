2b8b697a0b098604f248222bcbec36712538d061
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-09-07 23:44:56 +0000

    descrição das alterações realizadas

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ receitas = [
 
          'nome': 'torta de frango',
          'ingredientes':['500 g de peito de frango sem pele', '4 colheres (sopa) de óleo', '1 cebola picada', '1 xícara (chá) de ervilhas', 'pimenta-do-reino a gosto', '1/2 litro de caldo de galinha', '1 dente de alho amassado', '3 tomates sem pele e sem sementes', 'sal a gosto e pimenta', '250 ml de leite', '2 ovos','
-'sal e queijo ralado na gosto', '3/4 de xícara (chá) de óleo', '1 e 1/2 xícara (chá) de farinha de trigo', '1 colher (sopa) de fermento em pó'.] 
+'sal e queijo ralado na gosto', '3/4 de xícara (chá) de óleo', '1 e 1/2 xícara (chá) de farinha de trigo', '1 colher (sopa) de fermento em pó'] 
         'modo de preparo': ['Cozinhe o peito de frango no caldo até ficar macio. Separe 1 xícara (chá) de caldo do cozimento e reserve. Refogue os demais ingredientes e acrescente as ervilhas por último. Desfie o frango, misture ao caldo e deixe cozinhar até secar. Bata o leite, o óleo e os ovos no liquidificador em velocidade baixa. Acrescente aos poucos a farinha, o sal e o fermento. Despeje metade da massa em uma forma untada e adicione o recheio sobre ela. Cubra com o restante de massa e o queijo ralado. Leve ao forno preaquecido (180° C) até dourar.']
     },
     {
@@ -43,7 +43,7 @@ receitas = [
         'ingredientes':['500 g de farinha de trigo','1 copo de água',' 50 g de manteiga',' 1 colher de café de sal','25 g de fermento para pão','1 ovo','1 colher de sopa de fermento em pó','100 g de molho de tomate','1 linguiça calabresa','200 g de queijo mussarela ','orégano a gosto,rodelas de tomate e cebola .azeitona a gosto']
         'modo de preparo':['Misture 50 g de farinha, fermento e um pouco de água.Deixe descansar por 20 minutos. Adicione os demais ingredientes e amasse até obter uma massa bem macia.Corte em 3 partes iguais, forme 3 bolas e cubra-as com um pano.Deixe descansar por 1 hora. Abra a massa com um rolo até obter 25 cm de diâmetro. Espalhe o molho de tomate por cima e leve para assar em forno pré-aquecido, a 180ºC, por 15 minutos.Retire e deixe esfriar.Espalhe a linguiça sobre o molho de tomate e cubra com a cebola e o tomate.Coloque o queijo mussarela por baixo da linguiça.Decore com a azeitona e orégano.Retorne ao forno por mais 10 minutos ou até a mussarela dourar.']
     }
-]
+  ]
 ...
     
 @app.get("/")
@@ -70,4 +70,5 @@ def create_receita(dados: Receita):
     receitas.append(nova_receita)
 
     return nova_receita
+    
     
