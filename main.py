da981fab0c8eadc089c33ed8863939597ddd244f
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-09-27 00:50:14 +0000

    descrição das alterações realizadas

--- main.py
+++ main.py
@@ -17,12 +17,16 @@ class CreateReceita(BaseModel) :
     modo_de_preparo: str
 
 receitas: List[Receita] = []
-
+proximo_id = 0
 
     
 @app.get("/")
 def hello():
     return{"title":"Livro de receitas"}
+
+@app.get("/receitas")
+def get_todas_receitas():
+    return receitas
 
 @app.get("/receitas/{receita}")
 def get_receita(receita:str):
@@ -31,40 +35,53 @@ def get_receita(receita:str):
             return r
     return {"error": "Receita não encontrada"}
 
-
-
-@app.get("/receitas")
-def get_todas_receitas():
-    return receitas
-
-@app.post("/receitas", response_model=Receita, status_code=201)
-def create_receita(dados: CreateReceita):
-    global proximo_id
-
-    for r in receitas:
-        if r.nome.lower() == dados.nome.lower():
-            raise HTTPException(status_code=400,detail="Já existe uma receita com esse nome")
-
-    nova_receita=Receita(id = proximo_id , nome = dados.nome, ingredientes= dados.ingredientes, modo_de_preparo= dados.modo_de_preparo)
-    
-    receitas.append(nova_receita)
-    proximo_id+= 1
-
-    return nova_receita
-    
 @app.get("/receitas/id/{id}")
 def get_receitas(id: int):
     for r in receitas:
         if r.id == id:
             return r
-        raise HTTPException(status_code=404, detail="Receita não encontrada")
+        return {"mensagem": "Receita não encontrada"}
 
-@app.put("/receitas/{id}",response_model=Receita)
+@app.post("/receitas")
+def create_receita(dados: CreateReceita):
+    global proximo_id
+
+    for r in receitas:
+        if r.nome.lower() == dados.nome.lower():
+            return {"mensagem":"Já existe uma receita com esse nome"}
+
+         if not (1 <= len(dados.ingredientes) <= 20):
+            return {"mensagem": "A receita deve ter de 1 a 20 ingredientes"}
+
+         if not (2 <= len(dados.nome) <= 50):
+            return {"mensagem": "O nome da receita deve ter de 2 a 50 caracteres"}
+
+    proximo_id+= 1
+    nova_receita=Receita(id = proximo_id , nome = dados.nome, ingredientes= dados.ingredientes, modo_de_preparo= dados.modo_de_preparo)
+    
+    receitas.append(nova_receita)
+    return nova_receita
+
+
+@app.put("/receitas/{id}")
 def update_receita(id: int, dados: CreateReceita):
 
     for i in range(len(receitas)):
 
         if receitas[i].id == id:
+           for r in receitas:
+             if r.nome.lower() == dados.nome.lower():
+                    return {"mensagem": "Já existe uma receita com esse nome"}
+                
+             if r.nome == "":
+                    return {"mensagem": "O nome não pode estar vazio"}
+
+             if not (1 <= len(dados.ingredientes) <= 20):
+                return {"mensagem": "A receita deve ter entre 1 e 20 ingredientes"}
+            
+             if not (2 <= len(dados.nome) <= 50):
+                return {"mensagem": "O nome da receita deve ter entre 2 e 50 caracteres"}
+
             receitas_atualizada = Receita(
                 id=id,
                 nome=dados.nome,
@@ -76,10 +93,7 @@ def update_receita(id: int, dados: CreateReceita):
                     return {"mensagem":"receita já existe"}
             if receitas_atualizada.nome=="":
                 return{"digite algo"}
-                
-            receitas[i] = (receitas_atualizada)
-            return receitas_atualizada
-            raise HTTPException(status_code=404, detail= "Receita não encontrada")
+            
 
 @app.delete("/receitas/{ìd}")
 def deletar_receita(id: int):
