b8060c64829be07f15f7d7e21af98045b29d2d96
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-09-27 00:30:22 +0000

    descrição das alterações realizadas

--- main.py
+++ main.py
@@ -18,45 +18,7 @@ class CreateReceita(BaseModel) :
 
 receitas: List[Receita] = []
 
-'''
-receitas = [
-    {
-        'nome': 'brigaeiro',
-        'ingredientes': ['1 colher de sopa de manteiga', '1 lata ou 395 gramas de leite condensado', '30 gramas de chocolate em pó', '150 gramas de granulado de chocolate'],
-        'modo de preparo': 'Em uma panela, mistura o leite condensado, chocolate em pó e manteiga. Cozinhe em fogo médio e mexa até que o brigadeiro comece a desgrudar da panela. deixe esfriar e faça pequenas bolas com a mão passando a massa no chocolate granulado.'
 
-    },
-    {
-
-        'nome': 'palha italiana',
-        'ingredientes': ['1 lata de leite condensado', '1/2 colher (sopa) de margarina', '8 colheres (sopa) de chocolate em pó', '1 pacote de biscoito maisena'],
-        'modo de preparo': ['Pique o biscoito em pedacinhos pequenos e reserve. Com o leite condensado, a margarina e o chocolate em pó, faça um brigadeiro. Assim que o brigadeiro começar a soltar do fundo da panela, misture o biscoito picado até formar uma massa, retire do fogo. Unte uma bancada de mármore, ou alguma superfície lisa, com margarina e despeje essa massa. Abra a massa, batendo com a palma das mãos. Deixe esfriar e corte em quadradinhos.']
-
-    },
-    {
-
-         'nome': 'torta de frango',
-         'ingredientes':['500 g de peito de frango sem pele', '4 colheres (sopa) de óleo', '1 cebola picada', '1 xícara (chá) de ervilhas', 'pimenta-do-reino a gosto', '1/2 litro de caldo de galinha', '1 dente de alho amassado', '3 tomates sem pele e sem sementes', 'sal a gosto e pimenta', '250 ml de leite', '2 ovos','
-'sal e queijo ralado na gosto', '3/4 de xícara (chá) de óleo', '1 e 1/2 xícara (chá) de farinha de trigo', '1 colher (sopa) de fermento em pó'] 
-        'modo de preparo': ['Cozinhe o peito de frango no caldo até ficar macio. Separe 1 xícara (chá) de caldo do cozimento e reserve. Refogue os demais ingredientes e acrescente as ervilhas por último. Desfie o frango, misture ao caldo e deixe cozinhar até secar. Bata o leite, o óleo e os ovos no liquidificador em velocidade baixa. Acrescente aos poucos a farinha, o sal e o fermento. Despeje metade da massa em uma forma untada e adicione o recheio sobre ela. Cubra com o restante de massa e o queijo ralado. Leve ao forno preaquecido (180° C) até dourar.']
-    },
-    {
-        'nome': 'morango do amor',
-        'ingredientes':[' 2 xícaras e 1/2 de açúcar','  1 xícara e 1/2 de água','2 colheres de sopa de vinagre de álcool','Corante alimentício vermelho',' 12 morangos','3 colheres (sopa) de leite em pó','1 colher (sopa) de manteiga',' 1 lata de leite condensado',' 1/2 caixa de creme de leite'],
-        'modo de preparo':['Lave os morangos, remova o cabinho com as folhas e seque-os bem. Reserve.','Prepare o brigadeiro branco misturando leite condensado, leite em pó, creme de leite e manteiga em fogo médio','Mexa até engrossar, ficar bem consistente e soltar do fundo da panela.','Deixe esfriar completamente coberto com plástico filme','Separe bolinhas de brigadeiro e abra na mão até formar um disco. Você pode untar as mãos para não grudar.Coloque o morango no meio e puxe a massa do brigadeiro em volta até grudar e cobrir o morango por inteiro.','Enrole na mão para deixar bem uniforme.','Espete no palito sem atravessar e deixe na geladeira por 10 minutos enquanto prepara a calda para manter firme.Para a calda de caramelo, misture açúcar, água, vinagre e corante antes de ligar o fogo.  Depois ligue em fogo e não mexa mais.','Cozinhe em fogo médio para baixo por cerca de 15 minutos até borbulhar e chegar em ponto de bala dura. Se tiver um termômetro culinário, deve atingir 150ºC. Caso não tenha, teste pingando uma gota do caramelo em um copo com água fria e veja se endurece.Tire a calda de caramelo do fogo e cubra os morangos com ela, escorra o excesso e coloque-os sobre um recipiente com papel-manteiga ou untado.Deixe secar e pronto! Se quiser pode tirar do palito depois ou comer assim mesmo.]
-    },
-    {
-       'nome':'lasanha',
-       'ingreientes':['500 g de massa de lasanha',' 2 caixas de creme de leite','3 colheres de farinha de trigo','500 g de mussarela',' 2 copos de leite','3 colheres de óleo','3 dentes de alho amassado','500 g de carne moída',' 3 colheres de manteiga','500 g de presunto','sal a gosto','1 cebola ralada','1 caixa de molho de tomate',' 1 pacote de queijo ralado']
-       'modo de preparo':['Cozinhe a massa segundo as orientações do fabricante, despeje em um refratário com água gelada para não grudar e reserve.Refogue o alho, a cebola, a carne moída, o molho de tomate, deixe cozinhar por 3 minutos e reserve.Derreta a margarina, coloque as 3 colheres de farinha de trigo e mexa.Despeje o leite aos poucos e continue mexendo.Por último, coloque o creme de leite, mexa por 1 minuto e desligue o fogo.',' Despeje uma parte do molho à bolonhesa em um refratário, a metade da massa, a metade do presunto, a metade da mussarela, todo o molho branco e o restante da massa. Repita as camadas até a borda do recipiente.Finalize com o queijo ralado e leve ao forno alto (220° C), preaquecido, por cerca de 20 minutos.']
-    },
-    {
-        'nome': 'pizza',
-        'ingredientes':['500 g de farinha de trigo','1 copo de água',' 50 g de manteiga',' 1 colher de café de sal','25 g de fermento para pão','1 ovo','1 colher de sopa de fermento em pó','100 g de molho de tomate','1 linguiça calabresa','200 g de queijo mussarela ','orégano a gosto,rodelas de tomate e cebola .azeitona a gosto']
-        'modo de preparo':['Misture 50 g de farinha, fermento e um pouco de água.Deixe descansar por 20 minutos. Adicione os demais ingredientes e amasse até obter uma massa bem macia.Corte em 3 partes iguais, forme 3 bolas e cubra-as com um pano.Deixe descansar por 1 hora. Abra a massa com um rolo até obter 25 cm de diâmetro. Espalhe o molho de tomate por cima e leve para assar em forno pré-aquecido, a 180ºC, por 15 minutos.Retire e deixe esfriar.Espalhe a linguiça sobre o molho de tomate e cubra com a cebola e o tomate.Coloque o queijo mussarela por baixo da linguiça.Decore com a azeitona e orégano.Retorne ao forno por mais 10 minutos ou até a mussarela dourar.']
-    }
-  ]
-'''
     
 @app.get("/")
 def hello():
