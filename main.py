18de1aa54dc90489f937c7ae41b9a2ef8eea96ad
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-10-22 21:13:08 +0000

    descrição das alterações realizadas

--- main.py
+++ main.py
@@ -18,40 +18,41 @@ def hello():
 
 
 @app.get("/receitas")
-@app.get("/receitas", status_code=HTTPStatus.Ok)
+@app.get("/receitas",response_model=List[Receita], status_code=HTTPStatus.Ok)
 def get_todas_receitas():
     return receitas
 
 
-@app.get("/receitas/{receita}")
+@app.get("/receitas/{receita}", response_model=Receita, status_code=HTTPStatus.Ok)
 def get_receita(receita: str):
     for r in receitas:
         if r.nome.lower() == receita.lower():
             return r
-    return {"error": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
 
-@app.get("/receitas/id/{id}")
+@app.get("/receitas/id/{id}" , response_model=Receita, status_code=HTTPStatus.Ok)
 def get_receitas(id: int):
     for r in receitas:
         if r.id == id:
             return r
-    return {"mensagem": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
 
-@app.post("/receitas")
+@app.post("/receitas", response_model=Receita, status_code=HTTPStatus.Ok)
 def create_receita(dados: CreateReceita):
     global proximo_id
 
     for r in receitas:
         if r.nome.lower() == dados.nome.lower():
-            return {"mensagem": "Já existe uma receita com esse nome"}
+             raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Já existe uma receita com esse nome")
+
 
     if not (1 <= len(dados.ingredientes) <= 20):
-        return {"mensagem": "A receita deve ter de 1 a 20 ingredientes"}
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="A receita deve ter de 1 a 20 ingredientes")
 
     if not (2 <= len(dados.nome) <= 50):
-        return {"mensagem": "O nome da receita deve ter de 2 a 50 caracteres"}
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="O nome da receita deve ter de 2 a 50 caracteres")
 
     proximo_id += 1
     nova_receita = Receita(
@@ -65,22 +66,23 @@ def create_receita(dados: CreateReceita):
     return nova_receita
 
 
-@app.put("/receitas/{id}")
+@app.put("/receitas/{id}" , response_model=Receita, status_code=HTTPStatus.Ok)
 def update_receita(id: int, dados: CreateReceita):
     for i in range(len(receitas)):
         if receitas[i].id == id:
             for r in receitas:
                 if r.nome.lower() == dados.nome.lower():
-                    return {"mensagem": "Já existe uma receita com esse nome"}
-
+                    raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="Já existe uma receita com esse nome")
+                    
             if dados.nome == "":
                 return {"mensagem": "O nome não pode estar vazio"}
 
             if not (1 <= len(dados.ingredientes) <= 20):
-                return {"mensagem": "A receita deve ter entre 1 e 20 ingredientes"}
+                raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="A receita deve ter de 1 a 20 ingredientes")
 
             if not (2 <= len(dados.nome) <= 50):
-                return {"mensagem": "O nome da receita deve ter entre 2 e 50 caracteres"}
+                raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="O nome da receita deve ter de 2 a 50 caracteres")
+
 
             receita_atualizada = Receita(
                 id=id,
@@ -91,10 +93,10 @@ def update_receita(id: int, dados: CreateReceita):
             receitas[i] = receita_atualizada
             return receita_atualizada
 
-    return {"mensagem": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
 
 
-@app.delete("/receitas/{id}")
+@app.delete("/receitas/{id}" , response_model=Receita, status_code=HTTPStatus.Ok)
 def deletar_receita(id: int):
     if not receitas:
         return {"mensagem": "Não existe receita para apagar"}
@@ -105,4 +107,4 @@ def deletar_receita(id: int):
             receitas.pop(i)
             return {"mensagem": f"Receita {rct} deletada"}
 
-    return {"mensagem": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Receita não encontrada")
