95fed980f3fc93eebd66b7392cb7f7d88091c9b4
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-11-18 11:27:39 +0000

    aletrei

--- models.py
+++ models.py
@@ -5,15 +5,15 @@ from sqlalchemy.orm import Mapped, mapped_as_dataclass, mapped_column, registry
 
 table_registry = registry()
 
-mapped@_as_dataclass(table_registry)
+@mapped_as_dataclass(table_registry)
 class User:
     __tablename__ = 'users'
 
  id: Mapped[int] = mapped_column(init=False, primary_key=True)
-    nome_usuario: Mapped[str]= mapped_clumn(unique=True)
-    senha: Mapped[str]
-    email: Mapped[str] = mapped_column(unique=True)
-    created_at: Mapped[datetime] = mapped_column
-    (init=False, server_default=func.now()
-    )
+ nome_usuario: Mapped[str]= mapped_clumn(unique=True)
+ senha: Mapped[str]
+ email: Mapped[str] = mapped_column(unique=True)
+ created_at: Mapped[datetime] = mapped_column(
+    init=False, server_default=func.now()
+ )
     
