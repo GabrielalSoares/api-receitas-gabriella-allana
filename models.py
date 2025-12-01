90020c21c3a2b7e4bcba0d504a337abbe23786ef
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-12-01 13:53:00 +0000

    alterei

--- models.py
+++ models.py
@@ -12,12 +12,9 @@ class User:
     nome_usuario: Mapped[str]= mapped_column(unique=True)
     senha: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
-    
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
-
-    
     updated_at: Mapped[datetime] = mapped_column(
         init=False, 
         server_default=func.now(), 
